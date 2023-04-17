188ae1dc426634103a72fe8421028a83676ddf7e
Isadora Nepomuceno <iisadoranepomuceno@gmail.com>
2023-04-17 13:30:36 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/coursemsb/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursemsb.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
